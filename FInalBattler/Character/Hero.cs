ec8955cdf21707462a8e23c0343a048f0910be3d
MerkFranc <francisco.ledezma0805@gmail.com>
2026-06-04 12:26:35 -0600

    Starter Code

--- FInalBattler/Character/Hero.cs
+++ FInalBattler/Character/Hero.cs
@@ -18,6 +18,73 @@ namespace FinalBattler.Character
 
         public Hero()
         {
+            Name = "Unknown";
+            Level = 0;
+            Health = 1;
+            Power = 1;
+            Luck = 1;
+            Mana = 1;
+            TotalHealth = 1;
+            TotalPower = 1;
+            TotalLuck = 1;
+            Items = new List<Item>();
+            Skills = new List<Skill>();
+            Spells = new List<Spell>();
+            Equipment = new List<Equipment>();
+        }
+
+        public void DisplayStats(bool showTotalStats = false)
+        {
+            if (showTotalStats)
+            {
+                Console.WriteLine($"--- {Name} (Total Stats) ---");
+                Console.WriteLine($"Level: {Level}");
+                Console.WriteLine($"Health: {TotalPower}");
+                Console.WriteLine($"Power: {TotalPower}");
+                Console.WriteLine($"Luck: {TotalLuck}");
+                Console.WriteLine($"Mana: {Mana}");
+            }
+            else
+            {
+                Console.WriteLine($"--- {Name} (Base Stats) ---");
+                Console.WriteLine($"Level: {Level}");
+                Console.WriteLine($"Health: {Health}");
+                Console.WriteLine($"Power: {Power}");
+                Console.WriteLine($"Luck: {Luck}");
+                Console.WriteLine($"Mana: {Mana}");
+            }
+        }
+
+        public void LevelUp()
+        {
+            Random random = new Random();
+            Level++;
+
+            switch (CombatClass)
+            {
+                case CombatClass.Warrior:
+                    Health += random.Next(10, 21);
+                    Power += random.Next(1, 4);
+                    Luck += random.Next(1, 4);
+                    break;
+                
+                case CombatClass.Wizard:
+                    Health += random.Next(1, 16);
+                    Power += random.Next(3, 6);
+                    Luck += random.Next(1, 4);
+                    break;
+                
+                case CombatClass.Rogue:
+                    Health += random.Next(1, 16);
+                    Power += random.Next(1, 4);
+                    Luck += random.Next(3, 6);
+                    break;
+            }
+        }
+
+        public void CalculateTotals()
+        {
+            
         }
     }
 }
